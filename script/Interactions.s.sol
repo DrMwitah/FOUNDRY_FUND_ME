8a6ecb9327d68045397a215ee6661ebe3f12b200
Job Mwitaha <jobmwitah@gmail.com>
2023-06-18 18:45:47 +0300

    Mnabore

---
+++ script/Interactions.s.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+
+contract FundDundMe is Script {
+    function run() external {}
+}
+
+contract WithdrawFundMe is Script {}
